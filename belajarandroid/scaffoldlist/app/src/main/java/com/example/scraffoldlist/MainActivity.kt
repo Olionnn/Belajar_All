9e783c893f13885cff22f419d1d5312beb4d31d8
Olionnn\naya <cendekiaid2019@gmail.com>
2022-11-05 22:09:03 +0700

    Android (kotlin) [Scaforld dan SnackView Down]

--- belajarandroid/scaffoldlist/app/src/main/java/com/example/scraffoldlist/MainActivity.kt
+++ belajarandroid/scaffoldlist/app/src/main/java/com/example/scraffoldlist/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.scardfold_list
+package com.example.scraffoldlist
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -15,94 +15,126 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+
 //import com.example.scardfold_list.ui.theme.Scardfold_listTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Scaffoldlayout()
-        }
-    }
-}
-
-@Composable
-fun Scaffoldlayout() {
-    val materialcolor = Color.Red
-    val scaffoldState = rememberScaffoldState()
-    var textFieldState by remember{
-        mutableStateOf("")
-    }
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        scaffoldState = scaffoldState,
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "APPBAR-List", color = Color.White)
+            val materialcolor = Color.Red
+            val scaffoldState = rememberScaffoldState()
+            var textFieldState by remember {
+                mutableStateOf("")
+            }
+            val scope = rememberCoroutineScope()
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState,
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(text = "APPBAR-List", color = Color.White)
+                        },
+                        backgroundColor = materialcolor
+                    )
                 },
-                backgroundColor = materialcolor
+                bottomBar = {
+                    BottomAppBar(
+                        backgroundColor = materialcolor,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Box(modifier = Modifier
+                                .clip(RoundedCornerShape(5.dp))
+                                .background(Color.Blue)
+                                .padding(10.dp)
+                                .clickable { }
+                            ) {
+                                Text(text = "1", color = Color.White)
+                            }
+                            Box(modifier = Modifier
+                                .clip(RoundedCornerShape(5.dp))
+                                .background(Color.Black)
+                                .padding(10.dp)
+                                .clickable { }
+                            ) {
+                                Text(text = "2", color = Color.White)
+                            }
+                            Box(modifier = Modifier
+                                .clip(RoundedCornerShape(5.dp))
+                                .background(Color.Green)
+                                .padding(10.dp)
+                                .clickable { }
+                            ) {
+                                Text(text = "3", color = Color.White)
+                            }
+                        }
 
-            )
-        },
-        bottomBar = {
-            BottomAppBar(backgroundColor = materialcolor, modifier = Modifier.fillMaxWidth()) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ){
-                    Box(modifier = Modifier
-                        .clip(RoundedCornerShape(5.dp))
-                        .background(Color.Blue)
-                        .padding(10.dp)
-                        .clickable { }
-                    ) {
-                        Text(text = "1", color = Color.White)
+
+
                     }
-                    Box(modifier = Modifier
-                        .clip(RoundedCornerShape(5.dp))
-                        .background(Color.Black)
-                        .padding(10.dp)
-                        .clickable { }
+                },
+//                floatingActionButton = {
+//                    FloatingActionButton(onClick = {
+//                        scope.launch { scaffoldState.snackbarHostState.showSnackbar("Hallo $textFieldState") }
+//                    }) {
+//                        Text(text = "+")
+//                    }
+//                },
+//                floatingActionButtonPosition = FabPosition.End,
+//                drawerContent = {
+//                    Text(text = "Drawer 1 ")
+//                },
+                content = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(horizontal = 30.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
                     ) {
-                        Text(text = "2", color = Color.White)
-                    }
-                    Box(modifier = Modifier
-                        .clip(RoundedCornerShape(5.dp))
-                        .background(Color.Green)
-                        .padding(10.dp)
-                        .clickable { }
-                    ) {
-                        Text(text = "3", color = Color.White)
+                        Text(text = "abc")
+                        TextField(
+                                value = textFieldState,
+                        onValueChange = {
+                            textFieldState = it
+                        },
+                        label = {
+                            Text(text = "Masukan Nama Anda")
+                        },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(15.dp))
+                        Button(onClick = {
+                            scope.launch {
+                                scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
+                            }
+                        }) {
+
+                        }
                     }
                 }
+            )
+//            TextField(value =textFieldState , onValueChange = "" ,label = {
+//
+//            })
 
-
+//                        Text(text = "DRWARAWijirwj")
+                        // Untuk Content / isi Dalam ScarFOld bisa menghapus {} / Mengunakan Fungtiom Content =
+                    }
             }
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = { /*TODO*/ }) {
-                Text(text = "+")
-            }
-        },
-        floatingActionButtonPosition = FabPosition.End,
-        drawerContent = {
-            Text(text = "Drawer 1 ")
-        },
-        // content = { Text(text "aokwoakwokwao")}
-    ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center, modifier = Modifier
-            .fillMaxSize()
-            .padding(
-                horizontal = 30.dp
-            )) {
-//            TextField(value =textFieldState, label =)
-        }
-        Text(text = "DRWARAWijirwj")
-        // Untuk Content / isi Dalam ScarFOld bisa menghapus {} / Mengunakan Fungtiom Content =
     }
-}
+
+
+
+
+
 
