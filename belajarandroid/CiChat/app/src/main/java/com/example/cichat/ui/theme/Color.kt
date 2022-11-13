7196aa02c22dd7c86760230e9956e834fa73e51a
Olionnn\naya <cendekiaid2019@gmail.com>
2022-11-14 03:02:39 +0700

    Add New Color For Dark Theme And Light Theme

--- belajarandroid/CiChat/app/src/main/java/com/example/cichat/ui/theme/Color.kt
+++ belajarandroid/CiChat/app/src/main/java/com/example/cichat/ui/theme/Color.kt
@@ -1,8 +1,19 @@
 package com.example.cichat.ui.theme
 
+import android.graphics.Color.rgb
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+// Dark Theme
+val Black333 = Color(rgb(44, 51, 51))
+val DeepGreen395 = Color(rgb(57, 91, 100))
+val LightGreenA5C = Color(rgb(165, 201, 202))
+val LightBF2 = Color(rgb(231, 246, 242))
+// Light Theme
+val LightGreen = Color(rgb(207, 245, 231))
+val GreenAsL = Color(rgb(160, 228, 203))
+val AlGreenDark = Color(rgb(89, 193, 189))
+val BlueDarker = Color(rgb(89, 193, 189))
--- belajarandroid/CiChat/app/src/main/java/com/example/cichat/ui/theme/Theme.kt
+++ belajarandroid/CiChat/app/src/main/java/com/example/cichat/ui/theme/Theme.kt
@@ -7,15 +7,17 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Black333,
+    primaryVariant = DeepGreen395,
+    secondary = LightGreenA5C,
+    secondaryVariant = LightBF2
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = LightGreen,
+    primaryVariant = GreenAsL,
+    secondary = AlGreenDark,
+    secondaryVariant = BlueDarker,
 
     /* Other default colors to override
     background = Color.White,
