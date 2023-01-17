8aca204b22fa836e55fd1bf229e617e96029c564
OLIONNN\nayas <nayasasuke12@gmail.com>
2023-01-17 08:55:01 +0700

    Belajar Activity LifeCycle Done

---
+++ belajarandroid/udmey/anuskacourse/StarterProject/ActivityLifeCycleDemo/app/src/main/java/com/anushka/activitylifecycledemo/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.anushka.activitylifecycledemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("MYTAG", "MainActivity : OnCreate")
+        setContentView(R.layout.activity_main)
+        val greetingTextView = findViewById<TextView>(R.id.tvHello)
+        val inputField = findViewById<EditText>(R.id.etName)
+        val submitButton = findViewById<Button>(R.id.btnSubmit)
+        val offersButton = findViewById<Button>(R.id.btnOffers)
+        var enteredName = ""
+        submitButton.setOnClickListener {
+            enteredName = inputField.text.toString()
+            if (enteredName == ""){
+                offersButton.visibility = View.INVISIBLE
+                greetingTextView.text = ""
+                Toast.makeText(
+                    this@MainActivity,
+                    "Please, enter your name!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }else {
+                val message = "Welcome $enteredName"
+                greetingTextView.text = message
+                inputField.text.clear()
+                offersButton.visibility = View.VISIBLE
+            }
+        }
+
+        offersButton.setOnClickListener {
+            val intent = Intent(this,SecondActivity::class.java)
+            intent.putExtra("USER",enteredName)
+            startActivity(intent)
+        }
+
+
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MYTAG", "MainActifity : OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MYTAG", "MainActifity : OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MYTAG", "MainActifity : OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MYTAG", "MainActifity : OnStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MYTAG", "MainActifity : OnDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("MYTAG", "MainActifity : OnRestart")
+    }
+}
